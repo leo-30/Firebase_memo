ecc1b50a6c0a7659e35d22cc08f066386ed3527b
leo-30 <mio.rabbit971.snow@gmail.com>
2019-06-19 17:55:24 +0900

    Initial Commit

---
+++ Forebase_memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forebase_memo
+//
+//  Created by 原田澪 on 2019/06/19.
+//  Copyright © 2019 原田澪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
